baf75552cedc5362c175b646ae6d4dd4dbc6963a
Kim Fehrs <kim@github.com>
2016-04-19 14:13:05 +0200

    Chaotischer Commit.
    Generierung einer TileMap als Grid.
    Click Events zum erstellen von Türmen

---
+++ Assets/Script/ClickableTile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickableTile : MonoBehaviour {
+
+    public int tileX;
+    public int tileY;
+    public TileMap tilemap;
+
+    void OnMouseUp()
+    {
+        Debug.Log("Click!");
+        tilemap.CreateTurretUnit(tileX, tileY);
+    }
+}
