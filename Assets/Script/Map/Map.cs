7ed4336cc21e2aa3b8b6c31b8792f4076c67ac9b
Kim Fehrs <kofl91@github.com>
2016-05-23 19:19:05 +0200

    Added the possibility to place turrets via a generated TileGrid.

--- Assets/Script/Map/Map.cs
+++ Assets/Script/Map/Map.cs
@@ -2,15 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-
-[System.Serializable]
-public class SpawnPoint 
-{
-    public Transform self;
-    public Transform destination;
-}
-
-public abstract class Map : MonoBehaviour
+public abstract class Map : MonoSingleton<Map>
 {
 
     public enum eTileType
@@ -22,23 +14,16 @@ public abstract class Map : MonoBehaviour
 
     public GameObject container;
 
-    public List<SpawnPoint> spawnPoints = new List<SpawnPoint>();
-
-    public List<Waypoint> wayPoints = new List<Waypoint>();
-
     protected eTileType[,] grid;
 
-    protected int mapSizeX;
-    protected int mapSizeY;
+    public int mapSizeX;
+    public int mapSizeY;
 
-    private int creationCursorX;
-    private int creationCursorY;
-
-    private GameObject lastWaypoint;
+    public int tileSize;
+    public int xOffset = 0;
+    public int yOffset = 0;
 
     public bool isLoaded = true;
-
-
 
     void Start()
     {
@@ -59,56 +44,6 @@ public abstract class Map : MonoBehaviour
         }
     }
 
-    protected void createSpawnPoint(int x, int y, Transform firstDestination)
-    {
-        GameObject go = (GameObject)Instantiate(PrefabContainer.Instance.spawnPointPrefab, new Vector3(x, 1.0f, y), Quaternion.identity);
-        SpawnPoint sp = new SpawnPoint();
-        sp.self = go.transform;
-        sp.destination = firstDestination;
-        spawnPoints.Add(sp);
-        go.transform.parent = container.transform;
-    }
-
-    protected void createWayPoint(int x, int y)
-    {
-        
-        lastWaypoint = (GameObject)Instantiate(PrefabContainer.Instance.wayPointPrefab, new Vector3(x, 1.5f, y), Quaternion.identity);
-        Waypoint wp = lastWaypoint.GetComponent<Waypoint>();
-        wayPoints.Add(wp);
-        lastWaypoint.transform.parent = container.transform;
-    }
-
-    protected GameObject createEndZone(int x, int y)
-    {
-        GameObject go = (GameObject)Instantiate(PrefabContainer.Instance.endZonePrefab, new Vector3(x, 1.0f, y), Quaternion.identity);
-        go.transform.parent = container.transform;
-        return go;
-    }
-
-    protected void startWayFromTo (int x, int y, int xt, int yt)
-    {
-        setWayFromTo(x, y, xt, yt);
-        createWayPoint(xt, yt);
-        createSpawnPoint(x, y, lastWaypoint.transform);
-        creationCursorX = xt;
-        creationCursorY = yt;
-    }
-
-    protected void continueWayTo(int x, int y)
-    {
-        GameObject buffer = lastWaypoint;
-        createWayPoint(x, y);
-        buffer.GetComponent<Waypoint>().Destination = lastWaypoint.transform;
-        setWayFromTo(x, y, creationCursorX, creationCursorY);
-        creationCursorX = x;
-        creationCursorY = y;
-    }
-
-    protected void endWayAt(int x, int y)
-    {
-        setWayFromTo(x, y, creationCursorX, creationCursorY);
-        lastWaypoint.GetComponent<Waypoint>().Destination = createEndZone(x, y).transform;
-    }
 
     protected void setWayFromTo(int x1, int y1, int x2, int y2)
     {
@@ -145,7 +80,8 @@ public abstract class Map : MonoBehaviour
             {
                 TileType tt = PrefabContainer.Instance.tileTypes[(int)grid[x, y]];
 
-                GameObject go = (GameObject)Instantiate(tt.tileVisualPrefab, new Vector3(x, 1.0f, y), Quaternion.identity);
+                GameObject go = (GameObject)Instantiate(tt.tileVisualPrefab, new Vector3(x* tileSize+ xOffset, 1.0f, y* tileSize+ yOffset), Quaternion.identity);
+                go.transform.localScale += new Vector3(tileSize, 0, tileSize);
                 go.transform.parent = container.transform;
                 if (grid[x, y] != eTileType.Way)
                 {
@@ -158,80 +94,5 @@ public abstract class Map : MonoBehaviour
         }
     }
     #endregion
-
-
-
-    #region EnemySpawning
-    private List<GameObject> activeEnemys = new List<GameObject>();
-
-    private eTileType[,] Grid
-    {
-        get
-        {
-            return grid;
-        }
-
-        set
-        {
-            grid = value;
-        }
-    }
-
-    public void Spawn(int spawnPrefabIndex)
-    {
-        for( int i=0; i < spawnPoints.Count; i++)
-        {
-            Spawn(spawnPrefabIndex, i);
-        }
-    }
-
-    public void Spawn(int spawnPrefabIndex, int spawnPointIndex)
-    {
-        GameObject go = Instantiate(PrefabContainer.Instance.enemys[spawnPrefabIndex]
-            , spawnPoints[spawnPointIndex].self.position
-            , spawnPoints[spawnPointIndex].self.rotation) as GameObject;
-        go.transform.parent = container.transform;
-        go.SendMessage("SetDestination", spawnPoints[spawnPointIndex].destination);
-        activeEnemys.Add(go);
-        go.SendMessage("SetPlayer", GameManager.Instance.firstPlayer);
-    }
-
-    public void Despawn(GameObject go)
-    {
-        activeEnemys.Remove(go);
-        Destroy(go);
-    }
-
-
-    public void ClearEnemys()
-    {
-        foreach (GameObject go in activeEnemys)
-        {
-            Destroy(go);
-        }
-        activeEnemys.Clear();
-    }
-
-
-    public int getEnemysLeft()
-    {
-        return activeEnemys.Count;
-    }
-
-
-    #endregion
-
-
-    public float lastTime = 0.0f;
-    public float interval = 1.0f;
-
-    public void Update()
-    {
-        if ((Time.time - lastTime) >= interval)
-        {
-            //GameManager.Instance.currentMap.Spawn(0 , 0);
-            lastTime = Time.time;
-        }
-    }
     
 }
