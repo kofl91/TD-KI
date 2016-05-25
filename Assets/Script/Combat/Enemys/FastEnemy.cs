c36fb4a23662663dcd622a053de7c6ceb933c387
cebo92 <jan-cebo@hotmail.de>
2016-05-25 16:17:01 +0200

    Refactored the Enemys and added DamageTypes

--- Assets/Script/Combat/Enemys/FastEnemy.cs
+++ Assets/Script/Combat/Enemys/FastEnemy.cs
@@ -3,27 +3,10 @@ using System.Collections;
 
 public class FastEnemy : Enemy {
 
-    // Use this for initialization
-    void Start()
+    // Initialize
+    FastEnemy()
     {
         speed = 10f;
         life = 2f;
-        
-    }
-    
-    void ReachedGoal()
-    {
-
-        Destroy(gameObject);
-    }
-
-    public void OnDamage(DamageInfo damage)
-    {
-        life -= damage.amount;
-        if (life <= 0)
-        {
-            GameManager.Instance.firstPlayer.enemyKilled++;
-            Destroy(this.gameObject);
-        }
-    }
+    }  
 }
