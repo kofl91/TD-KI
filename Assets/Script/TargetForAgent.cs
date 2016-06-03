26142ab908a77060d45edbebf762abcfa1fc2199
Kim Fehrs <kofl91@github.com>
2016-06-03 14:04:47 +0200

    Added Asset and Scene for a 2 Player Map

---
+++ Assets/Script/TargetForAgent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetForAgent : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.position);
+	}
+}
