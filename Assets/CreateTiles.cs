e50be6523c5878c1abe6ea0bb4f3b2894abade76
kofl91 <kofl91@github.com>
2016-06-03 20:41:03 +0200

    Added a Script that adds Tiles dependent on TerrainHeight under a plane. This makes it very easy to create a grid over every Terrain, thus removing the time-cost of creating new MapScripts.

---
+++ Assets/CreateTiles.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// WORKS ONLY IF GAMEOBJECT IS A PLANE!!!
+public class CreateTiles : MonoBehaviour {
+
+    public GameObject tile;
+
+	// Use this for initialization
+	void Start () {
+        RaycastHit hit;
+        Vector3 runPosi;
+        runPosi = transform.position;
+        // Find a corner of the plane.
+        runPosi.x -= transform.lossyScale.x * 5;
+        runPosi.z -= transform.lossyScale.z * 5;
+        // Adjust for the size of a Tile
+        runPosi.x += 0.5f;
+        runPosi.z += 0.5f;
+        for (int x = 0; x < 10*transform.lossyScale.x; x++)
+        {
+            for (int y = 0; y < 10 * transform.lossyScale.z; y++)
+            {
+                Vector3 deltaPosi;
+                
+                deltaPosi.x = x;
+                deltaPosi.z = y;
+                deltaPosi.y = 2.0f;
+                Vector3 spawnPosi = runPosi + deltaPosi;
+                spawnPosi.y = 2.0f;
+                if (Terrain.activeTerrain.SampleHeight(spawnPosi) > 1.5f)
+                {
+                    GameObject newTile = Instantiate(tile, spawnPosi, tile.transform.rotation) as GameObject;
+                    newTile.transform.parent = transform;
+                }
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
--- Assets/Script/Map/Grid/ClickableTile.cs
+++ Assets/Script/Map/Grid/ClickableTile.cs
@@ -6,8 +6,27 @@ public class ClickableTile : MonoBehaviour {
     public int tileX;
     public int tileY;
 
+    void Start()
+    {
+        Debug.Log("I exist");
+    }
+
+
     void OnMouseUp()
     {
-        GameManager.Instance.SendMessage("TileClicked",this);
+        //GameManager.Instance.SendMessage("TileClicked",this);
+        Debug.Log("Clicked");
+    }
+
+    void OnMouseOver()
+    {
+        Debug.Log("Over");
+        GetComponent<MeshRenderer>().enabled = true;
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("Out");
+        GetComponent<MeshRenderer>().enabled = false;
     }
 }
