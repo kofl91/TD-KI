b2850415f9e929537f40ec74282b19e85cb74a54
kofl91 <kofl91@github.com>
2016-06-03 20:51:53 +0200

    Made an example in the 2 Player Map Scene where a turret shoots at NavMeshAgent-Enemys.
    Created a new Class BaseEnemy. This could replace the old Enemy Class making it easy to make the other enemys work on the new Map.

---
+++ Assets/Script/Combat/Enemys/BaseEnemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseEnemy : MonoBehaviour
+{
+
+    public float life = 1f;
+    public int bounty = 1;
+
+    public GameObject target;
+
+    protected DamageInfo dmgfactor = new DamageInfo();
+
+    public BaseEnemy()
+    {
+        dmgfactor.setNeutralResistance();
+    }
+
+    void Start()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+    }
+
+    protected void ReachedGoal()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnDamage(DamageInfo damage)
+    {
+        life -= damage.calcAbsoluteDmg(dmgfactor);
+        if (life <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
