5f661641799aee61d1c13f49805b710e35d7e543
cebo92 <jan-cebo@hotmail.de>
2016-08-23 10:30:02 +0200

    Mergi merg merg

---
+++ Assets/InfoScript.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfoScript : MonoBehaviour {
+
+    public GameObject bluePanel;
+    public GameObject redPanel;
+    public GameObject greenPanel;
+    public GameObject buffPanel;
+    public GameObject normalPanel;
+    public GameObject priestPanel;
+
+  
+    public void InfoBlue()
+    {
+
+        bluePanel.SetActive(true);
+        redPanel.SetActive(false);
+        greenPanel.SetActive(false);
+        buffPanel.SetActive(false);
+        normalPanel.SetActive(false);
+        priestPanel.SetActive(false);
+    }
+
+    public void InfoRed()
+    {
+
+        bluePanel.SetActive(false);
+        redPanel.SetActive(true);
+        greenPanel.SetActive(false);
+        buffPanel.SetActive(false);
+        normalPanel.SetActive(false);
+        priestPanel.SetActive(false);
+    }
+
+    public void InfoGreen()
+    {
+
+        bluePanel.SetActive(false);
+        redPanel.SetActive(false);
+        greenPanel.SetActive(true);
+        buffPanel.SetActive(false);
+        normalPanel.SetActive(false);
+        priestPanel.SetActive(false);
+    }
+    public void InfoNormal()
+    {
+
+        bluePanel.SetActive(false);
+        redPanel.SetActive(false);
+        greenPanel.SetActive(false);
+        buffPanel.SetActive(false);
+        normalPanel.SetActive(true);
+        priestPanel.SetActive(false);
+    }
+
+    public void InfoPriest()
+    {
+
+        bluePanel.SetActive(false);
+        redPanel.SetActive(false);
+        greenPanel.SetActive(false);
+        buffPanel.SetActive(false);
+        normalPanel.SetActive(false);
+        priestPanel.SetActive(true);
+    }
+
+    public void InfoBuff()
+    {
+
+        bluePanel.SetActive(false);
+        redPanel.SetActive(false);
+        greenPanel.SetActive(false);
+        buffPanel.SetActive(true);
+        normalPanel.SetActive(false);
+        priestPanel.SetActive(false);
+    }
+
+
+
+
+    void Update () {
+	
+    
+	}
+}
---
+++ Assets/Script/UI/GameOverScreen/GameOverEnemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverEnemy : MonoBehaviour {
+
+    public Transform endPoint;
+    NavMeshAgent agent;
+    void Start()
+    {
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        agent.SetDestination(endPoint.position);
+        if (agent.remainingDistance <= 3 && agent.remainingDistance != 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
